0af852444a0ec6309654fc844f524340774c4f99
Sander Bylemans <sbylemans@gmail.com>
2025-09-17 00:00:36 +0200

    DAG

--- transform-parquet-to-csv-v1.0.1.py
+++ transform-parquet-to-csv-v1.0.1.py
@@ -36,4 +36,6 @@ def transform_parquet_to_csv():
         df = pd.read_parquet('s3://titanic/passengers.parquet', storage_options=storage_options)
         df.to_csv('s3://titanic/passengers.csv', storage_options=storage_options)
     
+    transform()
+
 transform_parquet_to_csv()
