0b5d3d683f86b0deaac61b263f510aef719d309c
Sander Bylemans <sbylemans@gmail.com>
2025-10-15 18:26:50 +0200

    test-spark

--- spark.py
+++
@@ -1,6 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.3.6,software.amazon.awssdk:bundle:2.33.13").appName("CSV to Parquet").getOrCreate()
-
-df = spark.read.csv("s3a://test/test.csv", header=True, inferSchema=True)
-df.write.mode("overwrite").parquet("s3a://test/test.parquet")
